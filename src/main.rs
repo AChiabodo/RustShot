b54b335a7cd171c6e552a7190a137999c003f16f
alessandro <alessandro.chiabodo@gmail.com>
2023-07-23 18:51:56 +0200

    RF (GUI - Screen) - Added modules for GUI and Screen

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
-
-fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder());
-    let data = 0_u32;
-    AppLauncher::with_window(main_window)
-        .log_to_console()
-        .launch(data)
-}
-
-fn ui_builder() -> impl Widget<u32> {
-    // The label text will be computed dynamically based on the current locale and count
-    let text =
-        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
-    let label = Label::new(text).padding(5.0).center();
-    let button_increment = Button::new("increment")
-        .on_click(|_ctx, data, _env| *data += 1)
-        .padding(5.0);
-    let button_decrement = Button::new("decrement")
-        .on_click(
-            |_ctx, data, _env| -> () {
-            if *data <= 0 {
-                *data = 0
-            } else {
-                *data -= 1
-            }
-        }
-        )
-        .padding(5.0);
-
-    Flex::column()
-        .with_child(label)
-        .with_child(button_increment)
-        .with_child(button_decrement)
+mod screen;
+mod gui;
+fn main()  {
+    let temp = screen::display_list();
+    for (i,dspl) in temp.iter().enumerate(){
+        println!("Display {} : {} x {}",i,dspl.height(),dspl.width());
+    }
+    gui::main_window().unwrap();
 }
