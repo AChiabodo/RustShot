de09c88b31bd7bf26b5948ddb97931c2d08a3a26
alessandro <alessandro.chiabodo@gmail.com>
2023-08-09 11:08:15 +0200

    Feat(window) - added confirmation window before exit

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,19 +1,22 @@
-use std::cmp::max;
 use crate::screen::{self, display_list, take_screenshot};
-use eframe::egui::{Align, Button, CentralPanel, ColorImage, ComboBox, Context, ImageButton, ImageData, Layout, Pos2, Rect, Response, ScrollArea, Sense, TopBottomPanel};
+use eframe::egui::{
+    Align, Button, CentralPanel, ColorImage, ComboBox, Context, ImageButton, ImageData, Layout,
+    Pos2, Rect, Response, ScrollArea, Sense, TopBottomPanel, Window, Key,
+};
+use eframe::epaint::Color32;
 use eframe::{run_native, NativeOptions};
 use eframe::{App, Frame};
 use egui_extras::RetainedImage;
 use image::{DynamicImage, EncodableLayout, GenericImage, ImageBuffer, Rgb, Rgba};
-use scrap::Display;
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::thread;
-use std::time::Duration;
 use imageproc::definitions::Image;
 use imageproc::drawing;
 use imageproc::drawing::Canvas;
 use imageproc::point::Point;
-
+use scrap::Display;
+use std::cmp::max;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
+use std::time::Duration;
 
 fn select_display(index: usize) -> Option<Display> {
     let mut iter = screen::display_list().into_iter().enumerate();
@@ -80,6 +83,8 @@ struct RustShot {
     crop_info: CropState,
     paint_info: PaintState,
     action: Action,
+    show_confirmation_dialog: bool,
+    allowed_to_close: bool,
 }
 
 impl RustShot {
@@ -108,6 +113,8 @@ impl RustShot {
                 curr_ptr: Pos2::default(),
             },
             action: Action::None,
+            allowed_to_close: true,
+            show_confirmation_dialog: false,
         }
     }
     /// Used to restore state of the application when stopping the crop action for some reason
@@ -127,6 +134,7 @@ impl RustShot {
                     let paint_btn = ui.add(Button::new("Paint"));
                     if screenshot_btn.clicked() {
                         //Hide the application window
+                        self.allowed_to_close = false;
                         frame.set_visible(false);
                         let tx = self.sender.clone();
                         let c = ctx.clone();
@@ -135,7 +143,8 @@ impl RustShot {
                         //Thread that manages screenshots
                         thread::spawn(move || {
                             thread::sleep(Duration::from_millis(300));
-                            let current_display = select_display(value as usize).expect("Cannot select the correct display");
+                            let current_display = select_display(value as usize)
+                                .expect("Cannot select the correct display");
                             let screenshot = take_screenshot(current_display).unwrap();
                             match tx.send(screenshot) {
                                 //Force update() to be called again, so that the application window is made visible again. (when it's not visible otherwise update won't be called)
@@ -144,11 +153,12 @@ impl RustShot {
                             }
                         });
                     }
-                    if screenshot_save_btn.clicked() {
+
+                    if screenshot_save_btn.clicked() ||  ctx.input(|i| i.key_pressed(Key::F1)) {
                         match &self.screenshot {
                             Some(screenshot) => {
                                 let path =
-                                    tinyfiledialogs::save_file_dialog("Select save location", "./");
+                                    tinyfiledialogs::save_file_dialog("Select save location", "./screen.jpg");
                                 match path {
                                     Some(path) => {
                                         match image::save_buffer(
@@ -168,45 +178,45 @@ impl RustShot {
                             None => {}
                         }
                     }
+                    let mut selected = 0;
+                    ComboBox::from_label("Select Display")
+                        .selected_text(format!("{:?}", self.display.unwrap()))
+                        .show_ui(ui, |ui| {
+                            for (i, display) in screen::display_list().into_iter().enumerate() {
+                                if ui
+                                    .selectable_value(
+                                        &mut self.display.clone().unwrap(),
+                                        i,
+                                        format!(
+                                            "Display {} - {}x{} px",
+                                            i,
+                                            display.width(),
+                                            display.height()
+                                        ),
+                                    )
+                                    .clicked()
+                                {
+                                    selected = i;
+                                    println!("Selected : {}", selected);
+                                    self.display = Some(selected);
+                                }
+                            }
+                        });
+
                     if crop_btn.clicked() {
                         self.action = Action::Crop;
                     }
                     if paint_btn.clicked() {
                         self.action = Action::Paint;
                     }
-                }
-                /*let mut value = 0;
-                let mut selected = 0;
-                let display_selector = ComboBox::from_label("Select Display")
-                    .selected_text(format!("{:?}", self.display.unwrap()))
-                    .show_ui(ui, |ui| {
-                        for (i, display) in screen::display_list().into_iter().enumerate() {
-                            if ui
-                                .selectable_value(
-                                    &mut self.display.clone().unwrap(),
-                                    i,
-                                    format!(
-                                        "Display {} - {}x{} px",
-                                        i,
-                                        display.width(),
-                                        display.height()
-                                    ),
-                                )
-                                .clicked()
-                            {
-                                selected = i;
-                                println!("Selected : {}", selected);
-                                self.display = Some(selected);
-                            }
-                        }
-                    });*/
-                else if self.action == Action::Crop {
+                } else if self.action == Action::Crop {
                     let undo_crop_btn = ui.add(Button::new("Stop cropping"));
                     if undo_crop_btn.clicked() {
                         //To restore image without cropping rect
                         self.restore_from_crop();
                     }
-                } else if self.action == Action::Paint {}
+                } else if self.action == Action::Paint {
+                }
             })
         });
     }
@@ -223,7 +233,10 @@ impl RustShot {
                             screenshot.as_bytes(),
                         ),
                     );
-                    let img = ui.add(ImageButton::new(retained_img.texture_id(ctx), retained_img.size_vec2()));
+                    let img = ui.add(
+                        ImageButton::new(retained_img.texture_id(ctx), retained_img.size_vec2())
+                            .frame(false),
+                    );
                     if self.action == Action::Crop {
                         self.crop_logic(img);
                     } else if self.action == Action::Paint {
@@ -231,20 +244,30 @@ impl RustShot {
                     }
                 });
             }
-            None => { ScrollArea::both().show(ui, |ui| ui.label("No screenshots yet")); }
+            None => {
+                ScrollArea::both().show(ui, |ui| ui.label("No screenshots yet"));
+            }
         });
     }
 
     /// Logic for cropping the image
     fn crop_logic(&mut self, img: Response) {
         if !self.crop_info.clicked && img.clicked() {
-            self.crop_info.start_ptr = into_relative_pos(img.interact_pointer_pos().unwrap(), img.rect);
+            self.crop_info.start_ptr =
+                into_relative_pos(img.interact_pointer_pos().unwrap(), img.rect);
             self.crop_info.clicked = true;
         } else if self.crop_info.clicked && img.clicked() {
-            self.crop_info.end_ptr = into_relative_pos(img.interact_pointer_pos().unwrap(), img.rect);
+            self.crop_info.end_ptr =
+                into_relative_pos(img.interact_pointer_pos().unwrap(), img.rect);
             self.crop_info.clicked = false;
-            let width = max(1, (self.crop_info.end_ptr.x - self.crop_info.start_ptr.x).abs() as i32);
-            let height = max(1, (self.crop_info.end_ptr.y - self.crop_info.start_ptr.y).abs() as i32);
+            let width = max(
+                1,
+                (self.crop_info.end_ptr.x - self.crop_info.start_ptr.x).abs() as i32,
+            );
+            let height = max(
+                1,
+                (self.crop_info.end_ptr.y - self.crop_info.start_ptr.y).abs() as i32,
+            );
             //Permits an easier selection when cropping, allowing to generate the crop area in all directions
             let mut start_ptr = self.crop_info.start_ptr;
             if self.crop_info.curr_ptr.x < self.crop_info.start_ptr.x {
@@ -253,7 +276,12 @@ impl RustShot {
             if self.crop_info.curr_ptr.y < self.crop_info.start_ptr.y {
                 start_ptr.y = self.crop_info.curr_ptr.y;
             }
-            let new_screen = self.final_screenshot.as_ref().unwrap().crop_imm(start_ptr.x as u32, start_ptr.y as u32, width as u32, height as u32);
+            let new_screen = self.final_screenshot.as_ref().unwrap().crop_imm(
+                start_ptr.x as u32,
+                start_ptr.y as u32,
+                width as u32,
+                height as u32,
+            );
             self.screenshot = Some(DynamicImage::from(new_screen.clone()));
             self.final_screenshot = Some(DynamicImage::from(new_screen));
             self.action = Action::None;
@@ -267,8 +295,14 @@ impl RustShot {
                 Some(pos) => into_relative_pos(pos, img.rect),
                 None => self.crop_info.curr_ptr,
             };
-            let width = max(1, (self.crop_info.curr_ptr.x - self.crop_info.start_ptr.x).abs() as i32);
-            let height = max(1, (self.crop_info.curr_ptr.y - self.crop_info.start_ptr.y).abs() as i32);
+            let width = max(
+                1,
+                (self.crop_info.curr_ptr.x - self.crop_info.start_ptr.x).abs() as i32,
+            );
+            let height = max(
+                1,
+                (self.crop_info.curr_ptr.y - self.crop_info.start_ptr.y).abs() as i32,
+            );
             //Permits an easier selection when cropping, allowing to generate the crop area in all directions
             let mut start_ptr = self.crop_info.start_ptr;
             if self.crop_info.curr_ptr.x < self.crop_info.start_ptr.x {
@@ -277,8 +311,12 @@ impl RustShot {
             if self.crop_info.curr_ptr.y < self.crop_info.start_ptr.y {
                 start_ptr.y = self.crop_info.curr_ptr.y;
             }
-            let new_screen: Image<Rgb<u8>> = drawing::draw_hollow_rect(self.final_screenshot.as_ref().unwrap().as_rgb8().unwrap(),
-                                                                       imageproc::rect::Rect::at(start_ptr.x as i32, start_ptr.y as i32).of_size(width as u32, height as u32), white);
+            let new_screen: Image<Rgb<u8>> = drawing::draw_hollow_rect(
+                self.final_screenshot.as_ref().unwrap().as_rgb8().unwrap(),
+                imageproc::rect::Rect::at(start_ptr.x as i32, start_ptr.y as i32)
+                    .of_size(width as u32, height as u32),
+                white,
+            );
             self.screenshot = Some(DynamicImage::from(new_screen));
         }
     }
@@ -287,11 +325,22 @@ impl RustShot {
     fn paint_logic(&mut self, img: Response) {
         if img.clicked() {
             self.paint_info.curr_ptr = into_relative_pos(img.hover_pos().unwrap(), img.rect);
-            self.screenshot.as_mut().unwrap().draw_pixel(self.paint_info.curr_ptr.x as u32, self.paint_info.curr_ptr.y as u32, Rgba([0u8, 0u8, 0u8, 255u8]));
+            self.screenshot.as_mut().unwrap().draw_pixel(
+                self.paint_info.curr_ptr.x as u32,
+                self.paint_info.curr_ptr.y as u32,
+                Rgba([0u8, 0u8, 0u8, 255u8]),
+            );
             //Draw 15x15 circle around the pointer
             let white = Rgb([255u8, 255u8, 255u8]);
-            let new_screen: Image<Rgb<u8>> = drawing::draw_filled_circle(self.screenshot.as_ref().unwrap().as_rgb8().unwrap(),
-                                                                          (self.paint_info.curr_ptr.x as i32, self.paint_info.curr_ptr.y as i32), 5, white);
+            let new_screen: Image<Rgb<u8>> = drawing::draw_filled_circle(
+                self.screenshot.as_ref().unwrap().as_rgb8().unwrap(),
+                (
+                    self.paint_info.curr_ptr.x as i32,
+                    self.paint_info.curr_ptr.y as i32,
+                ),
+                5,
+                white,
+            );
             self.screenshot = Some(DynamicImage::from(new_screen));
         } else {
             //Reset paint info (different from restoring the app from painting !!!)
@@ -314,9 +363,32 @@ impl App for RustShot {
         }
         self.render_top_panel(ctx, frame);
         self.render_central_panel(ctx, frame);
+        if self.show_confirmation_dialog {
+            // Show confirmation dialog:
+            Window::new("Do you want to quit?")
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        if ui.button("Cancel").clicked() {
+                            self.show_confirmation_dialog = false;
+                        }
+
+                        if ui.button("Yes!").clicked() {
+                            self.allowed_to_close = true;
+                            frame.close();
+                        }
+                    });
+                });
+        }
+    }
+    fn on_close_event(&mut self) -> bool {
+        if !self.allowed_to_close {
+            self.show_confirmation_dialog = true;
+        }
+        self.allowed_to_close
     }
 }
-
 pub fn main_window() -> eframe::Result<()> {
     let window_option = NativeOptions::default();
     run_native(
