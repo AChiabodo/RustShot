6e17b8e15f5a6f4db23588dfad52e0d6752b7ae0
Francesco Dente s313355 <s313355@studenti.polito.it>
2023-08-10 15:55:29 +0200

    feat (gui): added new painting functionalities and icons

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,16 +1,12 @@
 use crate::screen::{self, take_screenshot};
-use eframe::egui::{
-    Align, Button, CentralPanel, color_picker, ColorImage, ComboBox, Context, ImageButton, ImageData, KeyboardShortcut, Layout,
-    Pos2, Rect, Response, ScrollArea, Sense, Shape, TopBottomPanel, Window, Key, Modifiers, InputState, Ui};
-use eframe::epaint::Color32;
+use eframe::egui::{Align, Button, CentralPanel, ColorImage, ComboBox, Context, ImageButton, KeyboardShortcut, Layout, Pos2, Rect, Response, ScrollArea, Sense, Shape, TopBottomPanel, Window, Key, Modifiers, InputState, Ui, TextureId, Label, CursorIcon};
+
 use eframe::{run_native, NativeOptions};
 use eframe::{App, Frame};
 use egui_extras::RetainedImage;
-use image::{DynamicImage, EncodableLayout, GenericImage, ImageBuffer, Rgb, Rgba, RgbImage};
+use image::{DynamicImage, Rgb, Rgba, RgbImage};
 use imageproc::definitions::Image;
 use imageproc::drawing;
-use imageproc::drawing::Canvas;
-use imageproc::point::Point;
 use scrap::Display;
 use std::cmp::max;
 use std::collections::HashMap;
@@ -59,6 +55,7 @@ enum Tool {
     Arrow,
     HollowCircle,
     FilledCircle,
+    Eraser,
     None,
 }
 #[derive(PartialEq, Eq, PartialOrd, Ord,Hash)]
@@ -168,7 +165,33 @@ struct RustShot {
     show_confirmation_dialog: bool,
     allowed_to_close: bool,
     shortcuts : HashMap<KeyCommand,KeyboardShortcut>,
+    icons: HashMap<String,Result<RetainedImage, String>>,
+    tooltips: HashMap<String,String>,
 }
+
+/// Load in the application state the svg icons as RetainedImage, and also the correspondence between the backend name of the icon and its tooltip.
+fn load_icons() -> (HashMap<String,Result<RetainedImage, String>>, HashMap<String,String>) {
+    let mut icons_map = HashMap::new();
+    let mut tooltips_map = HashMap::new();
+    icons_map.insert("pencil-fill".to_string(), RetainedImage::from_svg_bytes("pencil-fill",include_bytes!("../../resources/pencil-fill.svg")));
+    tooltips_map.insert("pencil-fill".to_string(), "Pencil".to_string());
+    icons_map.insert("square-fill".to_string(), RetainedImage::from_svg_bytes("square-fill",include_bytes!("../../resources/square-fill.svg")));
+    tooltips_map.insert("square-fill".to_string(), "Filled Rectangle".to_string());
+    icons_map.insert("square".to_string(), RetainedImage::from_svg_bytes("square",include_bytes!("../../resources/square.svg")));
+    tooltips_map.insert("square".to_string(), "Hollow Rectangle".to_string());
+    icons_map.insert("circle-fill".to_string(), RetainedImage::from_svg_bytes("circle-fill",include_bytes!("../../resources/circle-fill.svg")));
+    tooltips_map.insert("circle-fill".to_string(), "Filled Circle".to_string());
+    icons_map.insert("circle".to_string(), RetainedImage::from_svg_bytes("circle",include_bytes!("../../resources/circle.svg")));
+    tooltips_map.insert("circle".to_string(), "Hollow Circle".to_string());
+    icons_map.insert("arrow-up-right".to_string(), RetainedImage::from_svg_bytes("arrow-up-right",include_bytes!("../../resources/arrow-up-right.svg")));
+    tooltips_map.insert("arrow-up-right".to_string(), "Arrow".to_string());
+    icons_map.insert("eraser-fill".to_string(), RetainedImage::from_svg_bytes("eraser-fill",include_bytes!("../../resources/eraser-fill.svg")));
+    tooltips_map.insert("eraser-fill".to_string(), "Eraser".to_string());
+    icons_map.insert("x-octagon".to_string(), RetainedImage::from_svg_bytes("x-octagon",include_bytes!("../../resources/x-octagon.svg")));
+    tooltips_map.insert("x-octagon".to_string(), "Stop using this tool".to_string());
+    return (icons_map, tooltips_map);
+}
+
 
 impl RustShot {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -182,6 +205,7 @@ impl RustShot {
         map.insert(KeyCommand::TakeScreenshot,KeyboardShortcut { modifiers: Modifiers::CTRL, key: Key::T });
         map.insert(KeyCommand::Crop,KeyboardShortcut { modifiers: Modifiers::CTRL, key: Key::C });
         map.insert(KeyCommand::Paint,KeyboardShortcut { modifiers: Modifiers::CTRL, key: Key::P });
+        let (icons_map, tooltips_map) = load_icons();
         RustShot {
             screenshot: None,
             final_screenshot: None,
@@ -206,6 +230,8 @@ impl RustShot {
             allowed_to_close: true,
             show_confirmation_dialog: false,
             shortcuts : map,
+            icons: icons_map,
+            tooltips: tooltips_map,
         }
     }
     /// Used to restore state of the application when stopping the crop action for some reason
@@ -333,7 +359,7 @@ impl RustShot {
                         self.restore_from_crop();
                     }
                 } else if self.action == Action::Paint {
-                    self.render_paint_tools(ui);
+                    self.render_paint_tools(ctx, ui);
                 }
             })
         });
@@ -365,16 +391,75 @@ impl RustShot {
         });
     }
 
-    fn render_paint_tools(&mut self, ui: &mut Ui) {
+    /// Renders an ImageButton using the svg corresponding to the given name, if the svg failed to load or the name does not correspond to any svg, it spawns a button with the name passed as parameter to icon_button
+    fn icon_button(&mut self, name:&str, ctx: &Context, ui: &mut Ui) -> Response {
+        match self.icons.get(name) {
+            Some(val) => {
+                match val {
+                    Ok(image) => {
+                        ui.add(ImageButton::new(image.texture_id(ctx), image.size_vec2())).on_hover_text(self.tooltips.get(name).unwrap_or(&"Error".to_string()))
+                    }
+                    Err(_) => {
+                        ui.add(Button::new(name))
+                    }
+                }
+            }
+            None => {
+                ui.add(Button::new(name))
+            }
+        }
+    }
+
+    /// Renders an icon using the svg corresponding to the given name, if the svg failed to load or the name does not correspond to any svg, it spawns a button with the name passed as parameter to icon
+    fn icon(&mut self, name:&str, ctx: &Context, ui: &mut Ui) -> Response {
+        match self.icons.get(name) {
+            Some(val) => {
+                match val {
+                    Ok(image) => {
+                        ui.image(image.texture_id(ctx), image.size_vec2()).on_hover_text(self.tooltips.get(name).unwrap_or(&"Error".to_string()))
+                    }
+                    Err(_) => {
+                        ui.add(Label::new(name))
+                    }
+                }
+            }
+            None => {
+                ui.add(Label::new(name))
+            }
+        }
+    }
+
+    /// Renders painting annotation tools when in paint mode
+    fn render_paint_tools(&mut self, ctx: &Context, ui: &mut Ui) {
         ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
             let save_paint_btn = ui.add(Button::new("Save changes"));
             let undo_paint_btn = ui.add(Button::new("Undo changes"));
-            let draw_btn = ui.add(Button::new("✏"));
-            let hollow_rect_btn = ui.add(Button::new("◻"));
-            let filled_rect_btn = ui.add(Button::new("◼"));
-            let hollow_circle_btn = ui.add(Button::new("⚪"));
-            let filled_circle_btn = ui.add(Button::new("⚫"));
+            let draw_btn = self.icon_button("pencil-fill", ctx, ui);
+            let hollow_rect_btn = self.icon_button("square", ctx, ui);
+            let filled_rect_btn = self.icon_button("square-fill", ctx, ui);
+            let hollow_circle_btn = self.icon_button("circle", ctx, ui);
+            let filled_circle_btn = self.icon_button("circle-fill", ctx, ui);
+            let arrow_btn = self.icon_button("arrow-up-right", ctx, ui);
+            let eraser_btn = self.icon_button("eraser-fill", ctx, ui);
             ui.color_edit_button_srgb(&mut self.paint_info.curr_color);
+            ui.label("Current tool:");
+            let curr_tool = match self.paint_info.curr_tool {
+                Tool::Drawing => self.icon("pencil-fill", ctx, ui),
+                Tool::HollowRect => self.icon("square", ctx, ui),
+                Tool::FilledRect => self.icon("square-fill", ctx, ui),
+                Tool::HollowCircle => self.icon("circle", ctx, ui),
+                Tool::FilledCircle => self.icon("circle-fill", ctx, ui),
+                Tool::Arrow => self.icon("arrow-up-right", ctx, ui),
+                Tool::Eraser => self.icon("eraser-fill", ctx, ui),
+                Tool::None => ui.add(Label::new("No tool selected")),
+            };
+            let rmv_tool_btn = self.icon_button("x-octagon", ctx, ui);
+
+
+
+            if rmv_tool_btn.clicked(){
+                self.paint_info.curr_tool = Tool::None;
+            }
             if save_paint_btn.clicked() {
                 self.action = Action::None;
                 self.save_paint_changes();
@@ -396,6 +481,9 @@ impl RustShot {
             }
             if filled_circle_btn.clicked() {
                 self.paint_info.curr_tool = Tool::FilledCircle;
+            }
+            if arrow_btn.clicked() {
+                self.paint_info.curr_tool = Tool::Arrow;
             }
         });
     }
@@ -503,6 +591,11 @@ impl RustShot {
         } else if img.drag_released() {
             self.intermediate_screenshot = Some(DynamicImage::from(self.screenshot.as_ref().unwrap().clone()));
             self.paint_info.soft_reset();
+        }
+        //Change cursor when using a tool
+        match self.paint_info.curr_tool {
+            Tool::None => {},
+            _ => {img.on_hover_cursor(CursorIcon::Crosshair);},
         }
     }
 }
